5931f191b7821e161593b9ea92630e16ec2cce58
403ABHIJEET <abhijeet8745@gmail.com>
2025-06-08 19:25:18 +0530

    some changes

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def handle_disconnect():
     # Optional: remove from connected_users
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    socketio.run(app, host='0.0.0.0', port=5000)
